fbce596a6045af4acd7f17a59f75e9f3b9e8baf6
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-09-20 22:20:27 +0300

    Test fix deploy

--- server/lib/simpleAPIServer.js
+++ server/lib/simpleAPIServer.js
@@ -105,7 +105,7 @@ export async function buildSimpleAPIServer(cg, db) {
     method: "GET",
     path: "/api/current-version",
     handler: async (request, h) => {
-      return { version: '1.2.5 [TRACKING ORDER PROGRESS]' };
+      return { version: '1.2.6 [TEST FIXING DEPLOY]' };
     }
   });
 
