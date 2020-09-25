650bc916c2e586c31f364a1b3591b1a3a39cc4f6
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-09-25 21:16:22 +0300

    Update version

--- server/lib/simpleAPIServer.js
+++ server/lib/simpleAPIServer.js
@@ -105,7 +105,7 @@ export async function buildSimpleAPIServer(cg, db) {
     method: "GET",
     path: "/api/current-version",
     handler: async (request, h) => {
-      return { version: '1.3.0 [PROCESSED LOGIC]' };
+      return { version: '1.3.1 [LINNWORKS NOTES]' };
     }
   });
 
