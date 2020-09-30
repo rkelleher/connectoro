188a0690b30488af6c36c44b15bddcf31fdf4d02
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-09-30 09:30:18 +0300

    Validation order params

--- client/src/app/main/order/Order.js
+++ client/src/app/main/order/Order.js
@@ -461,7 +461,69 @@ function OrderEasyncDetails({ order }) {
     );
 }
 
+function orderData(order) {
+    const { EASYNC, LINNW} = order.integrationData;
+    const { request } = order.easyncOrderStatus;
+
+    const data = {
+        orderSource: 'Manually', // change logic for more sources
+        dropshipper: 'Manually', // change logic for more dropshippers
+        trackerObtained: 'False',
+        orderSourceID: null,
+        retailer: null,
+        totalPaid: null, 
+        dropshipperAccount: null,
+        dropshipDate: null,
+        retailerOrderID: null
+    };
+
+    if (order.LINNW) {
+        data.orderSource = 'Linnworks'; // change logic for more sources
+        data.orderSourceID = LINNW.numOrderId; // change logic for more sources
+    }
+
+    if (EASYNC) {
+        data.dropshipper = 'Easync'; // change logic for more dropshippers
+
+        if (EASYNC.retailerCode) {
+            data.retailer = EASYNC.retailerCode.replace("_", "");
+        }
+    }
+
+    if (request) {
+        if (request.price_components && request.price_components.subtotal) {
+            data.totalPaid =  request.price_components.subtotal;
+        }
+
+        if (request.tracking) {
+            data.trackerObtained = 'True';
+        }
+
+        if (request.merchant_order_ids && request.merchant_order_ids.length) {
+            data.dropshipperAccount = request.merchant_order_ids[0].account;
+            data.dropshipDate = moment(request.merchant_order_ids[0].placed_at).format('DD MM YYYY');
+            data.retailerOrderID = request.merchant_order_ids[0].merchant_order_id;
+        }
+    }
+
+    return data;
+}
+
 function OrderStatus({ order }) {
+    const { message, status, processedOnSource, requestId, idempotencyKey } = order.easyncOrderStatus;
+
+    const {
+        orderSource, 
+        orderSourceID, 
+        dropshipper, 
+        retailer, 
+        totalPaid, 
+        trackerObtained,
+        dropshipperAccount,
+        dropshipDate,
+        retailerOrderID 
+    } = orderData(order);
+
     return (
         <Card className="orderStatus">
             <CardContent className="orderStatus-content">
@@ -471,33 +533,31 @@ function OrderStatus({ order }) {
                 <Typography className="flex pb-24">
                     <Icon className="orderStatus-icon">local_shipping</Icon>
                     <Typography variant="subtitle1">
-                        <Typography variant="h6" className="block orderStatus-tracker" >{order.easyncOrderStatus.status.replace("_", " ")}</Typography>
-                        <span class="font-bold">Message: </span> {order.easyncOrderStatus.message}
+                        <Typography variant="h6" className="block orderStatus-tracker" >{status.replace("_", " ")}</Typography>
+                        <span class="font-bold">Message: </span> {message}
                     </Typography>
                 </Typography>
                 <Typography variant="subtitle1" className="orderStatus-paragraph">
-                <p><span class="font-bold">Order Source:</span> {order.integrationData.LINNW ? 'Linnworks' : 'Manually'}</p>
-                {/* change logic for more sources */}
-                <p><span class="font-bold">Order Source ID: </span> {order.integrationData.LINNW.numOrderId ? order.integrationData.LINNW.numOrderId : null}</p>
-                <p><span class="font-bold">Processed On Source: </span> {order.easyncOrderStatus.processedOnSource.toString()}</p>
+                <p><span class="font-bold">Order Source:</span> {orderSource}</p>
+                <p><span class="font-bold">Order Source ID: </span> {orderSourceID}</p>
+                <p><span class="font-bold">Processed On Source: </span> {processedOnSource.toString()}</p>
                 </Typography>
                 <Typography variant="body1" className="orderStatus-paragraph">
-                <p><span class="font-bold">Dropshipper: </span> {order.integrationData.EASYNC ? 'Easync' : 'Manually'}</p>
-                {/* change logic for more sources */}
-                <p><span class="font-bold">Dropshipper Account: </span> {(order.easyncOrderStatus.request && order.easyncOrderStatus.request.merchant_order_ids[0].account) ? order.easyncOrderStatus.request.merchant_order_ids[0].account : null}</p>
+                <p><span class="font-bold">Dropshipper: </span> {dropshipper}</p>
+                <p><span class="font-bold">Dropshipper Account: </span> {dropshipperAccount}</p>
                 </Typography>
                 <Typography variant="body1" className="orderStatus-paragraph">
-                <p><span class="font-bold">Request ID: </span> {order.easyncOrderStatus.requestId}</p>
-                <p><span class="font-bold">Indempotency Key:</span> {order.easyncOrderStatus.idempotencyKey}</p>
+                <p><span class="font-bold">Request ID: </span> {requestId}</p>
+                <p><span class="font-bold">Indempotency Key:</span> {idempotencyKey}</p>
                 </Typography>
                 <Typography variant="body1" className="orderStatus-paragraph">
-                <p><span class="font-bold">Dropship Date: </span> {(order.easyncOrderStatus.request && order.easyncOrderStatus.request.merchant_order_ids[0].placed_at) ? moment(order.easyncOrderStatus.request.merchant_order_ids[0].placed_at).format('DD MM YYYY') : null}</p>
-                <p><span class="font-bold">Retailer: </span>{order.integrationData.EASYNC.retailerCode ? order.integrationData.EASYNC.retailerCode.replace("_", " ") : null}</p>
-                <p><span class="font-bold">Retailer Order ID: </span> {(order.easyncOrderStatus.request && order.easyncOrderStatus.request.merchant_order_ids[0].merchant_order_id) ? order.easyncOrderStatus.request.merchant_order_ids[0].merchant_order_id : null}</p>
-                <p><span class="font-bold">Total Paid: </span>{(order.easyncOrderStatus.request && order.easyncOrderStatus.request.price_components.subtotal) ? order.easyncOrderStatus.request.price_components.subtotal : null}</p>
+                <p><span class="font-bold">Dropship Date: </span> {dropshipDate}</p>
+                <p><span class="font-bold">Retailer: </span>{retailer}</p>
+                <p><span class="font-bold">Retailer Order ID: </span> {retailerOrderID}</p>
+                <p><span class="font-bold">Total Paid: </span>{totalPaid}</p>
                 </Typography>
                 <Typography variant="body1" className="orderStatus-paragraph">
-                <p><span class="font-bold">Tracker Obtained: </span>{(order.easyncOrderStatus.request && order.easyncOrderStatus.request.tracking) ? 'True' : 'False'}</p>
+                <p><span class="font-bold">Tracker Obtained: </span>{trackerObtained}</p>
                 </Typography>
             </CardContent>
         </Card>
