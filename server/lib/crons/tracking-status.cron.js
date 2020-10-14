0e636d5f6ff28996e97fb1051c3385d5066c0e93
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-10-15 00:41:39 +0300

    Try catch for tracking number cron

--- server/lib/crons/tracking-status.cron.js
+++ server/lib/crons/tracking-status.cron.js
@@ -4,7 +4,7 @@ import { getAccount, getIntegrationByType, getIntegrationCredential } from '../c
 import { getAwaitingTrackerOrders } from '../controllers/order.controller.js';
 import { EASYNC_INTEGRATION_TYPE,  EASYNC_TOKEN_CREDENTIAL_KEY, EASYNC_TRACKING_RESPONSE_TYPES } from "../integrations/easync/easync.js";
 import { getTrackingByRequestId } from "../integrations/easync/getEasyncOrdedStatus.js";
-import { LINNW_INTEGRATION_TYPE, markLinnworkOrderAsProcessed, sendTrackingNumberToLinnw } from '../integrations/linnworks.js';
+import { LINNW_INTEGRATION_TYPE, markLinnworkOrderAsProcessed, sendTrackingNumberToLinnw, makeLinnworksAPISession } from '../integrations/linnworks.js';
 import * as IntegrationUtil from '../utils/integration.util.js';
 
 export const TrackingStatusCron = (cg) => cron.schedule('0 */90 * * * *',  async () => {
@@ -15,69 +15,71 @@ export const TrackingStatusCron = (cg) => cron.schedule('0 */90 * * * *',  async
   const orders = await getAwaitingTrackerOrders();
 
   for (const order of orders) {
-    const { requestId } = order.easyncOrderStatus;
+    try {
+      const { requestId } = order.easyncOrderStatus;
 
-    if (!requestId) { 
-      continue; 
-    };
+      if (!requestId) {
+        continue;
+      };
 
-    const account = await getAccount(order.accountId);
-    const integration = await getIntegrationByType(
-      account,
-      EASYNC_INTEGRATION_TYPE
-    );
-    const token = getIntegrationCredential(
-      integration,
-      EASYNC_TOKEN_CREDENTIAL_KEY
-    );
-
-    const request = await getTrackingByRequestId(requestId, token);
-
-    if (request.type !== EASYNC_TRACKING_RESPONSE_TYPES.SUCCESS) {
-      continue;
-    }
-
-    order.easyncTracking.isObtained = true;
-    order.easyncTracking.status = request.result.status;
-    order.easyncTracking.trackingNumber = request.result.tracking.aquiline;
-
-    const { orderId } = order.integrationData[LINNW_INTEGRATION_TYPE];
-
-    if (!orderId) {
-      await order.save();
-      continue;
-    }
-
-    const linnwIntegration = IntegrationUtil.getIntegrationByType(account, LINNW_INTEGRATION_TYPE);
-    
-    if (!linnwIntegration .session) {
-      const linnworksSession = await makeLinnworksAPISession(
-        linnwIntegration.appId,
-        cg("LINNW_APP_SECRET"),
-        linnwIntegration.credentials && linnwIntegration.credentials.get("INSTALL_TOKEN")
+      const account = await getAccount(order.accountId);
+      const integration = await getIntegrationByType(
+        account,
+        EASYNC_INTEGRATION_TYPE
+      );
+      const token = getIntegrationCredential(
+        integration,
+        EASYNC_TOKEN_CREDENTIAL_KEY
       );
 
-      linnwIntegration.session = linnworksSession;
-      await account.save();
-    }
+      const request = await getTrackingByRequestId(requestId, token);
 
-    await sendTrackingNumberToLinnw(
-      linnwIntegration.session.Token,
-      orderId,
-      request.result.tracking.aquiline
-    );
+      if (request.type !== EASYNC_TRACKING_RESPONSE_TYPES.SUCCESS) {
+        continue;
+      }
 
-    const res = await markLinnworkOrderAsProcessed(
-      linnwIntegration.session.Token,
-      orderId
-    );
+      order.easyncTracking.isObtained = true;
+      order.easyncTracking.status = request.result.status;
+      order.easyncTracking.trackingNumber = request.result.tracking.aquiline;
 
-    if (!res) {
-      continue;
-    }
+      const { orderId } = order.integrationData[LINNW_INTEGRATION_TYPE];
 
-    order.easyncTracking.processedOnSource = res.Processed;
-    await order.save();
+      if (!orderId) {
+        await order.save();
+        continue;
+      }
+
+      const linnwIntegration = IntegrationUtil.getIntegrationByType(account, LINNW_INTEGRATION_TYPE);
+      
+      if (!linnwIntegration .session) {
+        const linnworksSession = await makeLinnworksAPISession(
+          linnwIntegration.appId,
+          cg("LINNW_APP_SECRET"),
+          linnwIntegration.credentials && linnwIntegration.credentials.get("INSTALL_TOKEN")
+        );
+
+        linnwIntegration.session = linnworksSession;
+        await account.save();
+      }
+        
+      await sendTrackingNumberToLinnw(
+        linnwIntegration.session.Token,
+        orderId,
+        request.result.tracking.aquiline
+      );
+
+      const res = await markLinnworkOrderAsProcessed(
+        linnwIntegration.session.Token,
+        orderId
+      );
+
+      if (!res) {
+        continue;
+      }
+
+      order.easyncTracking.processedOnSource = res.Processed;
+      await order.save();
+    } catch (error) {}
   }
 }, {
   timezone: 'Europe/Kiev',
