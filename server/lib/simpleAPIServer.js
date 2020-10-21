03b622546c70b4af4ad5526d79a0114c9ebe7a17
Anton Stroka <deestrok@gmail.com>
2020-10-21 10:28:25 +0200

    test cron3

--- server/lib/simpleAPIServer.js
+++ server/lib/simpleAPIServer.js
@@ -105,7 +105,7 @@ export async function buildSimpleAPIServer(cg, db) {
     method: "GET",
     path: "/api/current-version",
     handler: async (request, h) => {
-      return { version: '1.4.9 [Tested crons]' };
+      return { version: '1.4.10 [Tested crons v2]' };
     }
   });
 
