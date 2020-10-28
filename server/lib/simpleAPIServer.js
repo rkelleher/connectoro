6dc81d2a30d3e8ef7485928a60deaa0d7287bad9
Anton Stroka <deestrok@gmail.com>
2020-10-28 23:58:28 +0100

    cron 10 20 30 40 50 60  & validation 95%

--- server/lib/simpleAPIServer.js
+++ server/lib/simpleAPIServer.js
@@ -105,7 +105,7 @@ export async function buildSimpleAPIServer(cg, db) {
     method: "GET",
     path: "/api/current-version",
     handler: async (request, h) => {
-      return { version: '1.4.12 [default crons]' };
+      return { version: '1.4.13 [new crons]' };
     }
   });
 
