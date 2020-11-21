32665015322b24c4dd17d14d3a73ad4d2f808323
Maxe1 <hatamaxel@gmail.com>
2020-11-21 18:45:20 +0200

    Remove crons headers

--- server/lib/simpleAPIServer.js
+++ server/lib/simpleAPIServer.js
@@ -105,12 +105,7 @@ export async function buildSimpleAPIServer(cg, db) {
     method: "GET",
     path: "/api/current-version",
     handler: async (request, h) => {
-      if (user.role !== "admin" || request.get("X-Appengine-Cron")) {
-        return { version: '1.5.2 [DELETE EASYNC WEBHOOKS]' };
-      }
-      else{
-        return { version: '1.5.2 [DELETE EASYNC WEBHOOKS]' };
-      }
+      return { version: '1.5.2 [DELETE EASYNC WEBHOOKS]' };
     }
   });
 
