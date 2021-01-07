02d8efa3e38b4a01360af12eeb8cc8fd14d56f7d
Anton Stroka <deestrok@gmail.com>
2021-01-07 16:04:20 +0100

    socket fix v2

--- client/src/app/main/orders/OrdersTable.js
+++ client/src/app/main/orders/OrdersTable.js
@@ -56,7 +56,7 @@ function OrdersTable(props) {
     }, [orders]);
 
     useEffect(() => {
-        const io = socket('http://localhost:3000');
+        const io = socket('https://stage.connectoro.io');
         io.on('news', function (data) {
             console.log(data);
         });
